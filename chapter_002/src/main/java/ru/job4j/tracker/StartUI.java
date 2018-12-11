7ccdc0661c6f2fd18c88464f3327ed1ff12a33b2
imaginegeek57 <imaginegeek57@gmail.com>
2018-12-11 21:48:18 +0300

    1. Используя класс ConsoleInput в классе StartUI обеспечить полноценную работу всего приложения

--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -26,7 +26,8 @@ public class StartUI {
 
     /**
      * Конструтор инициализирующий поля.
-     * @param input ввод данных.
+     *
+     * @param input   ввод данных.
      * @param tracker хранилище заявок.
      */
     public StartUI(Input input, Tracker tracker) {
@@ -58,16 +59,33 @@ public class StartUI {
      */
     private void createItem() {
         System.out.println("------------ Добавление новой заявки --------------");
-        String name = this.input.ask("Введите имя заявки :");
-        String desc = this.input.ask("Введите описание заявки :");
-        Item item = new Item(name, desc);
+        String name = this.input.ask("Введите имя заявки");
+        String desc = this.input.ask("Введите описание заявки");
+        Item item = new Item(name,desc);
         this.tracker.add(item);
         System.out.println("------------ Новая заявка с getId : " + item.getId() + "-----------");
+    }
+    // метод удаляет заявку
+    private void delete() {
+        System.out.println("--------Удаление заявки--------");
+        String name = this.input.ask("Введите имя заявки :");
+        //.....?
+        this.tracker.delete(name);
+    }
+    private Item findById(String id) {
+        //...?
     }
 
     private void showMenu() {
         System.out.println("Меню.");
-        // добавить остальные пункты меню.
+        System.out.println("Добавление новой заявки");
+        System.out.println("поиск заявки по имени");
+        System.out.println("поиск заявки по id");
+        System.out.println("удаление заявки");
+        System.out.println("Меню.");
+        final String EXIT = "6";
+        final String ADD = "0";
+        // это не правильно ?
     }
 
     /**
