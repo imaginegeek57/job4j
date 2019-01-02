5df8f0b4b59bd51788c017b7b5f06a90e34504c8
imaginegeek57 <imaginegeek57@gmail.com>
2019-01-02 19:35:40 +0300

    5. Рефакторинг теста @Before @After

--- chapter_002/src/main/java/ru/job4j/oop/Triangle.java
+++ chapter_002/src/main/java/ru/job4j/oop/Triangle.java
@@ -1,10 +1,11 @@
 package ru.job4j.oop;
 
+
 public class Triangle implements Shape {
         public String draw() {
             StringBuilder pic = new StringBuilder();
-            pic.append("  ^  ");
-            pic.append(" ^ ^ ");
+            pic.append("  ^  ").append(System.lineSeparator());
+            pic.append(" ^ ^ ").append(System.lineSeparator());
             pic.append("^^^^^");
             return pic.toString();
     }
