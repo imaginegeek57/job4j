520e064e4ff49882ae471812f9f1a46c7f4e2fc7
imaginegeek57 <imaginegeek57@gmail.com>
2019-01-13 23:29:32 +0300

    3. if-else-throw

--- chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -5,8 +5,11 @@ package ru.job4j.tracker;
  * @since 0.1
  */
 import java.util.*;
+
 public class ConsoleInput implements Input {
+
     private Scanner scanner = new Scanner(System.in);
+
     public String ask(String question) {
         System.out.print(question);
         return scanner.nextLine();
@@ -15,15 +18,11 @@ public class ConsoleInput implements Input {
         int key = Integer.valueOf(this.ask(question));
         boolean exist = false;
         for (int value : range) {
-            if (value == key) {
-                exist = true;
-                break;
+            if (value != key) {
+                throw new MenuOutException("Out of menu range.");
             }
         }
-        if(exist) {
-            return key;
-        } else {
-            throw new MenuOutException("Out of menu range.");
-        }
+        exist = true;
+        return key;
     }
 }
