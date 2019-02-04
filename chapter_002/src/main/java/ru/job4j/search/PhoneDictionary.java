45a25b26a4b6a76505e8d9dc16a9941f230108d4
imaginegeek57 <imaginegeek57@gmail.com>
2019-02-04 19:44:03 +0300

    Телефонный справочник на базе ArrayList

--- chapter_002/src/main/java/ru/job4j/search/PhoneDictionary.java
+++ chapter_002/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -24,6 +24,7 @@ public class PhoneDictionary {
         List <Person> result = new ArrayList <>();
         for (Person person : persons)
             if (person.getName().contains(key) || person.getSurname().contains(key) || person.getPhone().contains(key) || person.getAddress().contains(key)) {
+             result.add(person);
             }
         return result;
     }
