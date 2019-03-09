433f830f652d6cc43c9273168b80936bd2ff94a5
imaginegeek57 <imaginegeek57@gmail.com>
2019-03-10 00:16:48 +0300

    2. Сортировка User с использованием Comparator

--- chapter_002/src/main/java/ru/job4j/collection/SortUser.java
+++ chapter_002/src/main/java/ru/job4j/collection/SortUser.java
@@ -4,32 +4,20 @@ package ru.job4j.collection;
  * @version $Id$
  * @since 0.1
  */
-import java.util.Comparator;
-        import java.util.Collection;
-        import java.util.List;
-        import java.util.Set;
-        import java.util.TreeSet;
+        import java.util.*;
 public class SortUser {
-    public Set<User> sort(List<User> user) {
-        Set<User> users = new TreeSet<>();
-        for (User use : users){
-            users.add(use);
-        }
-        return users;
+    public Set <User> sort(List <User> user) {
+        Set <User> users = new TreeSet <>();
+        return new TreeSet <>(user);
     }
-    public List<User> sortNameLength(List<User>user) {
-        Set<User> users = new TreeSet<>();
-        for (User use : user) {
-            user.sort(
-                    new Comparator <User>() {
-                        @Override
-                        public int compare(User o1, User o2) {
-                            return o1.getName().length() - o2.getName().length();
-                        }
-                    }
-            );
-            users.add(use);
-        }
-        return (List <User>) users;
+    public Set <User> sortName(List <User> user) {
+        user.sort(new Comparator <User>() {  // используем comparator что бы указать правила сортировки -
+            @Override                         // обьектов (user) в коллекции
+            public int compare(User o1, User o2) {
+                return o1.getName().compareTo(o2.getName());   // сравниваем два обьекта o1 b o2 по имени
+            }                                                 // и сортеруем их
+        });
+        return (Set <User>) user;
     }
 }
+
--- chapter_002/src/main/java/ru/job4j/collection/User.java
+++ chapter_002/src/main/java/ru/job4j/collection/User.java
@@ -4,26 +4,29 @@ package ru.job4j.collection;
  * @version $Id$
  * @since 0.1
  */
-import java.util.Comparator;
-public class User {
+import java.util.*;
+public class User implements Comparable<User> {
     private String name;
     private int age;
-    public User (String name, int age) {
+
+    public User(String name, int age) {
         this.name = name;
-        this.age =age;
+        this.age = age;
     }
+
     public String getName() {
         return this.name;
     }
+
     public int getAge() {
         return this.age;
     }
-}
-class UserAge implements Comparator<User> {
+
     @Override
-    public int compare(User o1, User o2) {
-        int comp = o1.getName().compareTo(o2.getName());
-        return comp !=0 ? comp : Integer.compare(o1.getAge(),o2.getAge());
+    public int compareTo(User o) {
+        return Integer.compare(this.age, o.age);
     }
 }
+
+
 
