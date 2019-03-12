603a5464aa94eb938a49adf4617f5b332946bb05
imaginegeek57 <imaginegeek57@gmail.com>
2019-03-12 07:52:28 +0300

    3. Компаратор для строк.

--- chapter_002/src/main/java/ru/job4j/collection/CompSort.java
+++ chapter_002/src/main/java/ru/job4j/collection/CompSort.java
@@ -11,11 +11,11 @@ public class ListCompare implements Comparator<String> {
     public int compare(String left, String right) {
         char[] l = left.toCharArray();
         char[] r = right.toCharArray();
-        int min = l.length - r.length;
-            for (int i : min) {
+        char[] min = (l.equals(r)) ? l : r;
+            for (int i = 0; i < min.length; i++) {
                 if(l[i] != r[i]) {
                 }
         }
-        return min;
+        return Character.compare(char left, char right);   // не знаю что вернуть...
     }
 }
