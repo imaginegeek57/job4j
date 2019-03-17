7d5b1d9c4a91bea6e57e9d2f2ed07d3258179191
imaginegeek57 <imaginegeek57@gmail.com>
2019-03-17 15:52:25 +0300

    Банковские переводы.

--- chapter_002/src/main/java/ru/job4j/Bank/Bank.java
+++ chapter_002/src/main/java/ru/job4j/Bank/Bank.java
@@ -34,19 +34,19 @@ public class Bank {
     public void addAccountToUser(String passport, Account account) {
         List <Account> accounts = null;
         for (User u : data.keySet()) {
-            if (u.getPassport().equals(passport))
-                data.put(u, new ArrayList <>());     // тут пробовал сделать по аналогу метода addUser
-        }                                             // но idea ругается..
+            if (u.getPassport().equals(account)) {
+                if (account == null) {
+                    data.put(u, new ArrayList <>());
+                }
+            }
+        }
     }
-
     // удалить один счёт пользователя.
     public void deleteAccountFromUser(String passport, Account account) {
         List <Account> accounts = null;
         for (User u : data.keySet()) {
-            if (u.getPassport().equals(passport)) {
-                data.remove(passport, account);     // здесь я удалил account по ключу (passport)
-            }
-
+           data.get(u).indexOf(account);
+            data.remove(account);
         }
     }
     public boolean transferMoney(User user1, Account account1,
