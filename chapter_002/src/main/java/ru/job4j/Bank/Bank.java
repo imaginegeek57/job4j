604c21d479763c2832025b1f5ba31d03f9e685a4
imaginegeek57 <imaginegeek57@gmail.com>
2019-03-24 22:40:47 +0300

    Банковские переводы.

--- chapter_002/src/main/java/ru/job4j/Bank/Bank.java
+++ chapter_002/src/main/java/ru/job4j/Bank/Bank.java
@@ -8,21 +8,17 @@ import java.util.*;
 public class Bank {
     public static void main(String[] args) {
     }
-
     public Map <User, List <Account>> data = new HashMap <>();
-
     // добавление пользователя.
     public void addUser(User user) {
         if (!data.containsKey(user)) {
             data.put(user, new ArrayList <>());
         }
     }
-
     // удаление пользователя.
     public void deleteUser(User user) {
         data.remove(user);
     }
-
     //получить список счетов для пользователя
     public List <Account> getUserAccounts(String passport) {
         List <Account> accounts = null;
@@ -34,7 +30,6 @@ public class Bank {
         }
         return accounts;
     }
-
     //добавить счёт пользователю.
     public void addAccountToUser(String passport, Account account) {
         for (User u : data.keySet()) {
@@ -47,7 +42,6 @@ public class Bank {
             }
         }
     }
-
     // удалить один счёт пользователя.
     public void deleteAccountFromUser(String passport, Account account) {
         for (User u : data.keySet()) {
@@ -80,13 +74,13 @@ public class Bank {
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String dstRequisite, double amount) {
         for (User u : data.keySet()) {
-            if (u.getPassport().equals(srcPassport) && u.getPassport().equals(destPassport)) {
-                List <Account> accounts = data.get(u);
-
+            if (data.get(u).contains(srcPassport) && data.get(u).contains(destPassport)) {
+                final List <Account> accounts = data.get(u);
+                getActualAccount(srcPassport, srcRequisite).transfer(
+                        getActualAccount(destPassport, dstRequisite), amount);
+                break;
             }
         }
-        return  getActualAccount(srcPassport, srcRequisite).transfer(
-                getActualAccount(destPassport, dstRequisite), amount);
+        return  true;
     }
 }
-
