bdd6182eb84defe04b8b3704533e319726c1a5dd
imaginegeek57 <imaginegeek57@gmail.com>
2019-03-29 22:26:48 +0300

    Home task

---
+++ chapter_002/src/main/java/ru/job4j/dop/Department.java
@@ -0,0 +1,17 @@
+package ru.job4j.dop;
+/**
+ * @author Vlad Mon (imaginegeek57@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+import java.util.*;
+public class Department {
+    private String name;
+
+    public Department(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return this.name;
+    }
+}
