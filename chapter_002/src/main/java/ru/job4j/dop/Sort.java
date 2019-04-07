ece38656670a65b35b565fc269d99b778ea4e9ff
imaginegeek57 <imaginegeek57@gmail.com>
2019-04-07 16:04:42 +0300

    Hometask

--- chapter_002/src/main/java/ru/job4j/dop/Sort.java
+++ chapter_002/src/main/java/ru/job4j/dop/Sort.java
@@ -2,28 +2,39 @@ package ru.job4j.dop;
 
 import java.util.*;
 
-public class Sort  {
+public class Sort {
     public static void main(String[] args) {
-        List <Department> data = new ArrayList<>();
-        data.addAll(
-                Arrays.asList(
-                        new Department("K1\\SK1"),
-                        new Department("K1\\SK2"),
-                        new Department("K1\\SK1\\SSK1"),
-                        new Department("K1\\SK1\\SSK2"),
-                        new Department("K2"),
-                        new Department("K2\\SK1\\SSK1"),
-                        new Department("K2\\SK1\\SSK2")
-                ));
-        System.out.println(data);
-        data.sort(
-                new Comparator <Department>() {
-                    @Override
-                    public int compare(Department o1, Department o2) {
-                        return o1.getName().compareTo(o2.getName());
-                    }
-                }
-        );
-            System.out.println(data);
     }
+
+    List <String> addMissedElement(String value) {
+    List <String> items = Arrays.asList(value.split("\\s*,\\s*"));
+
+    }
+}
+
+
+
+
+//        List<Department> data = new ArrayList<>();
+//        data.addAll(
+//                Arrays.asList(
+//                        new Department("K1\\SK1"),
+//                        new Department("K1\\SK2"),
+//                        new Department("K1\\SK1\\SSK1"),
+//                        new Department("K1\\SK1\\SSK2"),
+//                        new Department("K2"),
+//                        new Department("K2\\SK1\\SSK1"),
+//                        new Department("K2\\SK1\\SSK2")
+//                ));
+//        System.out.println(data);
+//        data.sort(
+//                new Comparator<Department>() {
+//                    @Override
+//                    public int compare(Department o1, Department o2) {
+//                        return o1.getName().compareTo(o2.getName());
+//                    }
+//                }
+//        );
+//            System.out.println(data);
+//    }
 }
