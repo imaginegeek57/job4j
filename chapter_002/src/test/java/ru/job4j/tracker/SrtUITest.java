dd12e164319555a9c8afb8adf8574f13d2aa6755
imaginegeek57 <imaginegeek57@gmail.com>
2019-04-14 23:29:11 +0300

    Test

--- chapter_002/src/test/java/ru/job4j/tracker/SrtUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/SrtUITest.java
@@ -9,8 +9,21 @@ import java.util.function.Consumer;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 public class SrtUITest {
-    String[] menu = {"0", "1", "2", "3", "4", "5", "6"};// я понимаю что это бред
     private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    final String  menu =  "0. Add new Item."
+            + System.lineSeparator()
+            + "1. Show all Item"
+            + System.lineSeparator()
+            + "2. Update Item."
+            + System.lineSeparator()
+            + "3. Delete Item."
+            + System.lineSeparator()
+            + "4. Find Item by ID"
+            + System.lineSeparator()
+            + "5. Find Item by name"
+            + System.lineSeparator()
+            + "6. Exit"
+            + System.lineSeparator();
     private final Consumer <String> output = new Consumer<String>() {
         private final PrintStream stdout = new PrintStream(out);
 
@@ -28,7 +41,7 @@ public class SrtUITest {
     public void whenFindById() {
         Tracker tracker = new Tracker();
         new StartUI(new StubInput(new String[] {"0","name", "desc", "n", "1", "y"}), tracker, output).init();
-        final Item item = tracker.findByName("name").get(0);
+        final Item item = tracker.findByName("name").get(0);// вот строка ожидания, как ни пытался ее добавить не получается
         System.out.println(item);
         assertThat(this.output.toString(), is(item));
     }
