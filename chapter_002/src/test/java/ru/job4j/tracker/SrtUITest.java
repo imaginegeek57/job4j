29237158b61e1212c566b124826755a696b30484
imaginegeek57 <imaginegeek57@gmail.com>
2019-04-14 18:34:57 +0300

    Test

--- chapter_002/src/test/java/ru/job4j/tracker/SrtUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/SrtUITest.java
@@ -9,20 +9,27 @@ import java.util.function.Consumer;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 public class SrtUITest {
+
     private final ByteArrayOutputStream out = new ByteArrayOutputStream();
     private final Consumer <String> output = new Consumer<String>() {
         private final PrintStream stdout = new PrintStream(out);
+        @Before
 
         @Override
         public void accept(String s) {
             stdout.println(s);
         }
+        @Override
+        public String toString() {
+            return out.toString();
+        }
     };
     @Test
     public void whenFindById() {
         Tracker tracker = new Tracker();
-        new StartUI(new StubInput(new String[] {"4"}), tracker, output).init();
-        String expected = output.accept("Find item by Id");
-        assertThat(this.output.toString(), is(expected));
+        new StartUI(new StubInput(new String[] {"0","name", "desc", "n", "1", "y"}), tracker, output).init();
+        final Item item = tracker.findByName("name").get(0);
+        System.out.println(item);
+        assertThat(this.output.toString(), is(item));
     }
 }
