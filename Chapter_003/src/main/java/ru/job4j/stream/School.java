ab467864c9eb30ca00f2899ce8b9ed51a7656c03
imaginegeek57 <imaginegeek57@gmail.com>
2019-04-19 07:55:57 +0300

    1. Фильтрация учеников.

--- Chapter_003/src/main/java/ru/job4j/stream/School.java
+++ Chapter_003/src/main/java/ru/job4j/stream/School.java
@@ -6,10 +6,10 @@ import java.util.List;
 
 import java.util.stream.Collectors;
 public class School {
-    List<Student> collect(List <Student> students, Predicat<Student>predict){ // Predicat - это опечатка или в этом есть смысл?
+    List<Student> collect(List <Student> students, Predicate<Student>predict){
         List<Student> classes = new LinkedList <>();
-        List<Student> splitUp = classes.stream().filter(student -> student.score.contains(classes) // почему contains не работает?
-        ).collect(Collectors.toList());
+        List<Student> splitUp = classes.stream().filter(predict)
+        .collect(Collectors.toList());
         return splitUp;
     }
 }
--- Chapter_003/src/main/java/ru/job4j/stream/Student.java
+++ Chapter_003/src/main/java/ru/job4j/stream/Student.java
@@ -1,5 +1,15 @@
 package ru.job4j.stream;
 
 public class Student {
-    int score;
+    private int score;
+    Student(int score) {
+        this.score = score;
+    }
+
+    public int getScore() {
+        return score;
+    }
+    public void setScore(int score) {
+        this.score = score;
+    }
 }
