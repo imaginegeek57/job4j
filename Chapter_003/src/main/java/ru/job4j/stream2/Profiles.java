ab779574afed11212336058676f0e5de19f441ff
imaginegeek57 <imaginegeek57@gmail.com>
2019-04-21 20:29:36 +0300

    2. Список адресов

--- Chapter_003/src/main/java/ru/job4j/stream2/Profiles.java
+++ Chapter_003/src/main/java/ru/job4j/stream2/Profiles.java
@@ -1,11 +1,17 @@
 package ru.job4j.stream2;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class Profiles {
-    List<Address>collect(List<Profile>profiles) {
-      // TO Do
-        return //something;
-    }
 
+    List<Address>collect(List<Profile>profiles) {
+        List <Profile> str = new ArrayList <>();
+        profiles.stream().map(profile -> profile.getAddress())
+                .collect(Collectors.toList());
+
+       return str;
+    }
 }
