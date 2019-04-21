77637d53275082c7831520b52676eb6104f16828
imaginegeek57 <imaginegeek57@gmail.com>
2019-04-21 14:14:59 +0300

    2. Список адресов

---
+++ Chapter_003/src/main/java/ru/job4j/stream2/Address.java
@@ -0,0 +1,38 @@
+package ru.job4j.stream2;
+
+public class Address {
+    private String city;
+    private String street;
+    private  int home;
+    private int apartment;
+    Address (String city, String street, int home, int apartment) {
+        this.city = city;
+        this.apartment = apartment;
+        this.street = street;
+        this.home = home;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public String getStreet() {
+        return street;
+    }
+    public void setStreet(String street) {
+        this.street = street;
+    }
+    public int getHome() {
+        return home;
+    }
+    public void setHome(int home) {
+        this.home = home;
+    }
+    public int getApartment() {
+        return apartment;
+    }
+    public void setApartment(int apartment) {
+        this.apartment = apartment;
+    }
+}
---
+++ Chapter_003/src/main/java/ru/job4j/stream2/Profiles.java
@@ -0,0 +1,11 @@
+package ru.job4j.stream2;
+
+import java.util.List;
+
+public class Profiles {
+    List<Address>collect(List<Profile>profiles) {
+      // TO Do
+        return //something;
+    }
+
+}
