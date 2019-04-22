ac5bfed0001245e18f081c6d0c22400f8a08c500
imaginegeek57 <imaginegeek57@gmail.com>
2019-04-22 22:11:41 +0300

    Test

--- Chapter_003/src/test/java/ru/job4j/stream2/ProfilesTest.java
+++ Chapter_003/src/test/java/ru/job4j/stream2/ProfilesTest.java
@@ -13,8 +13,19 @@ public class ProfilesTest {
         Profiles profiles = new Profiles();
         final Address address = new Address("Moscow", "Lenin", 25, 2);
         final Profile profile = new Profile(address);
-        List<Profile> str = Arrays.asList(profile);
-        List<Address> cool = profiles.collect(str);
+        List <Profile> str = Arrays.asList(profile);
+        List <Address> cool = profiles.collect(str);
         assertThat(cool, is(Arrays.asList(address)));
+    }
+    @Test
+    public void whenSomething() {
+        Profiles profiles = new Profiles();
+        final Address address = new Address("Spb", "North", 21, 5);
+        final Address address1 = new Address("Krsk", "West", 11, 7);
+        final Profile profile = new Profile(address);
+        final Profile profile1 = new Profile(address1);
+        List <Profile> str = Arrays.asList(profile, profile1);
+        List <Address> cool = profiles.collect(str);
+        assertThat(cool, is(Arrays.asList(address, address1)));
     }
 }
