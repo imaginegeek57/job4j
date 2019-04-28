d22ded5c774bee12e8d37364a6bd55fb10272cf7
imaginegeek57 <imaginegeek57@gmail.com>
2019-04-28 20:06:30 +0300

    6. Тестовое задание из модуля коллекции Lite переделать на Stream API

--- Chapter_003/src/main/java/ru/job4j/bankStream/Bank.java
+++ Chapter_003/src/main/java/ru/job4j/bankStream/Bank.java
@@ -7,30 +7,24 @@ public class Bank {
     }
     public Map <User, List <Account>> data = new HashMap <>();
     // добавление пользователя.
-    public void addUser1(User user) {
-        data.keySet().stream().filter(data -> data.equals(user)).collect(Collectors.toList());
+    public void addUser(User user) {
+        if (!data.containsKey(user)) {
+            data.put(user, new ArrayList <>());
+        }
     }
-//    public void addUser(User user) {
-//        if (!data.containsKey(user)) {
-//            data.put(user, new ArrayList <>());
-//        }
-//    }
     //получить список счетов для пользователя
     public List <Account> getUserAccounts1(String passport) {
-       data.keySet().stream().filter(data -> data.getPassport().equals(passport)).collect(Collectors.toList());
-       return getUserAccounts1(passport);
+       return data.keySet().stream().filter(data -> data.getPassport().equals(passport)).collect(Collectors.toList());
+
     }
     // удаление пользователя.
-    public void deleteUser1(User user) {
-        data.keySet().remove(user);            //нужно ли здест что-то расписывать?
+    public void deleteUser(User user) {
+        data.remove(user);
     }
-
-
-
-    // удаление пользователя.
-//    public void deleteUser(User user) {
-//        data.remove(user);
-//    }
+    //получить список счетов для пользователя
+    public List <Account> getUserAccounts(String passport) {
+        return data.keySet().stream().filter(data -> data.getPassport().equals(passport)).collect(Collectors.toList());
+    }
     //получить список счетов для пользователя
 //    public List <Account> getUserAccounts(String passport) {
 //        List <Account> accounts = null;
@@ -44,44 +38,67 @@ public class Bank {
 //    }
     //добавить счёт пользователю.
     public void addAccountToUser(String passport, Account account) {
-        for (User u : data.keySet()) {
-            if (u.getPassport().equals(passport)) {
-                final List <Account> accounts = data.get(u);
-                if (!accounts.contains(account)) {
-                    accounts.add(account);
-                    break;
-                }
-            }
-        }
+        data.keySet().stream().filter(data -> data.getPassport().equals(passport)).filter(data -> data.equals(account))
+                .collect(Collectors.toList());
+
     }
-    // удалить один счёт пользователя.
-    public void deleteAccountFromUser(String passport, Account account) {
-        for (User u : data.keySet()) {
-            if (u.getPassport().equals(passport)) {
-                final List <Account> accounts = data.get(u);
-                if (accounts.contains(account)) {
-                    accounts.remove(account);
-                    break;
-                }
-            }
-        }
-    }
+
+//    //добавить счёт пользователю.
+//    public void addAccountToUser(String passport, Account account) {
+//        for (User u : data.keySet()) {
+//            if (u.getPassport().equals(passport)) {
+//                final List <Account> accounts = data.get(u);
+//                if (!accounts.contains(account)) {
+//                    accounts.add(account);
+//                    break;
+//                }
+//            }
+//        }
+//    }
+         // удалить один счёт пользователя.
+       public void deleteAccountFromUser(String passport, Account account) {
+            (data.keySet().stream().filter(data -> data.getPassport().equals(passport)).collect(Collectors.toList());
+            data.keySet().removeIf(accounts -> accounts.equals(account));
+           }
+
+//    // удалить один счёт пользователя.
+//    public void deleteAccountFromUser(String passport, Account account) {
+//        for (User u : data.keySet()) {
+//            if (u.getPassport().equals(passport)) {
+//                final List <Account> accounts = data.get(u);
+//                if (accounts.contains(account)) {
+//                    accounts.remove(account);
+//                    break;
+//                }
+//            }
+//        }
+//    }
+       //поиска аккаунта по реквизитам и паспорту
+       public Account getActualAccount(String passport, String requisites) {
+        return data.keySet().stream().filter(data -> data.getPassport().equals(passport))
+                .filter(data -> data.equals(requisites)).collect(Collectors.toList());
+       }
+
+
+
+
+
     //поиска аккаунта по реквизитам и паспорту
-    public Account getActualAccount(String passport, String requisites) {
-        Account acc = null;
-        for (User u : data.keySet()) {
-            if (u.getPassport().equals(passport)) {
-                List <Account> accounts = data.get(u);
-                for (Account account : accounts) {
-                    if (account.getRequisites().contains(requisites)) {
-                        acc = account;
-                        break;
-                    }
-                }
-            }
-        }
-        return acc;
-    }
+//    public Account getActualAccount(String passport, String requisites) {
+////        Account acc = null;
+//        for (User u : data.keySet()) {
+//            if (u.getPassport().equals(passport)) {
+//                List <Account> accounts = data.get(u);
+//                for (Account account : accounts) {
+//                    if (account.getRequisites().contains(requisites)) {
+//                        acc = account;
+//                        break;
+//                    }
+//                }
+//            }
+//        }
+//        return acc;
+//    }
     //перевод денег
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String dstRequisite, double amount) {
