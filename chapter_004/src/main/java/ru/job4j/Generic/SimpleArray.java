dfff7c0003dc352170aaad587d5b861935d33c75
imaginegeek57 <imaginegeek57@gmail.com>
2019-05-04 19:39:34 +0300

    5.1.2. Создать итератор четные числа[#139062]

--- chapter_004/src/main/java/ru/job4j/Generic/SimpleArray.java
+++ chapter_004/src/main/java/ru/job4j/Generic/SimpleArray.java
@@ -2,6 +2,7 @@ package ru.job4j.Generic;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+import java.util.Objects;
 
 public class SimpleArray<T> implements Iterator<T> {
 
@@ -15,17 +16,21 @@ public class SimpleArray<T> implements Iterator<T> {
 
     public void add(T model) {
         this.objects[index++] = model;
-
     }
-//    public T set(int index, T model) {
-//        return (T) this.objects[index] = model;
-//    }
+    public T set(int index, T model) {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return (T) (this.objects[index] = model);
+    }
 
     public T get(int position) {
         return (T) this.objects[position];
     }
     @Override
     public boolean hasNext() {
+        if (objects == null || index < 0 || index >= objects.length ) {
+        }
         return index < this.objects.length;
     }
     @Override
@@ -35,8 +40,14 @@ public class SimpleArray<T> implements Iterator<T> {
         }
         return (T) this.objects[index++];
     }
-    @Override
-    public void remove() {
-
+    public T remove(int index) {
+        if (objects == null || index < 0 || index >= objects.length ) {
+        }
+        Objects[]result = new Objects[objects.length - 1];
+        System.arraycopy(objects, 0 ,result, 0, index);
+        if (objects.length != index) {
+            System.arraycopy(objects, index + 1, result, index, objects.length - index - 1);
+        }
+        return (T)result;
     }
 }
