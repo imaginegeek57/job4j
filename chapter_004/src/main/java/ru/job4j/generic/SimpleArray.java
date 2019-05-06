eac1c08ea772938ac8a679a9c655ac2e99199b23
imaginegeek57 <imaginegeek57@gmail.com>
2019-05-06 23:34:33 +0300

    5.2.1. Реализовать SimpleArray<T>[#139067]

---
+++ chapter_004/src/main/java/ru/job4j/generic/SimpleArray.java
@@ -0,0 +1,60 @@
+package ru.job4j.generic;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SimpleArray<T> implements Iterable<T> {
+
+    private Object[] objects;
+
+    private int index = 0;
+
+    public SimpleArray(int size) {
+        this.objects = new Object[size];
+    }
+
+    public void add(T model) {
+        this.objects[index++] = model;
+    }
+
+    public boolean set(int index, T model) {
+        if (index > -1 && index < this.index) {
+            throw new IndexOutOfBoundsException();
+        }
+        this.objects[index] = model;
+        return true;
+    }
+
+    public T get(int position) {
+        return (T) this.objects[position];
+    }
+
+    public T remove(int index) {
+        if (index > -1 && index < this.index) {
+            throw new IndexOutOfBoundsException();
+        }
+        final T res = (T) objects[index];
+        System.arraycopy(objects, index + 1, objects, index, objects.length - 1 - index);
+        this.objects[--index] = null;
+        return res;
+    }
+
+    public Iterator <T> iterator() {
+        return new Iterator <T>() {
+            int ind;
+            @Override
+            public boolean hasNext() {
+                return ind < index;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return (T) objects[ind++];
+            }
+        };
+    }
+}
--- chapter_004/src/test/java/ru/job4j/generic/SimpleArrayTest.java
+++ chapter_004/src/test/java/ru/job4j/generic/SimpleArrayTest.java
@@ -1,7 +1,6 @@
-package ru.job4j.Generic;
+package ru.job4j.generic;
 
 import org.junit.Test;
-
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
@@ -9,23 +8,25 @@ public class SimpleArrayTest {
 
     @Test
     public void whenCreateSomething() {
-        SimpleArray<String> simple = new SimpleArray(3);
-        simple.add("Hi");
-        simple.add("to");
-        simple.add("anyone");
-        String result = simple.get(2);
-        assertThat(result, is("anyone"));
-
-    }
-    @Test
-    public void whenCreateSomethingElse() {
-        SimpleArray<String> simple = new SimpleArray(5);
+        SimpleArray<String> simple = new SimpleArray(10);
         simple.add("Hi");
         simple.add("to");
         simple.add("anyone");
         simple.add("else");
-        simple.remove();
+        simple.add("123");
+        simple.remove(3);
         String result = simple.get(2);
         assertThat(result, is("anyone"));
+    }
+
+    @Test
+    public void whenCreateSomethingElse() {
+        SimpleArray<Integer> simple = new SimpleArray(10);
+        simple.add(3);
+        simple.add(7);
+        simple.add(12);
+        simple.set(1, 8);
+        Integer result = simple.get(1);
+        assertThat(result, is(8));
     }
 }
