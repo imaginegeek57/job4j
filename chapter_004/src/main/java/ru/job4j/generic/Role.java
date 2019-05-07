7e0fd95949db0c37884888d64e052ea9f8fae79c
imaginegeek57 <imaginegeek57@gmail.com>
2019-05-07 22:58:59 +0300

    5.2.2. Реализовать Store<T extends Base>[#139068]

---
+++ chapter_004/src/main/java/ru/job4j/generic/Role.java
@@ -0,0 +1,9 @@
+package ru.job4j.generic;
+
+public class Role extends Base {
+
+    protected Role(String id) {
+        super(id);
+    }
+
+}
---
+++ chapter_004/src/main/java/ru/job4j/generic/RoleStore.java
@@ -0,0 +1,5 @@
+package ru.job4j.generic;
+
+public class RoleStore {
+
+}
