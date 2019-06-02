0a306472901d88e457e0c9253a3867a3426483c9
imaginegeek57 <imaginegeek57@gmail.com>
2019-06-02 19:51:52 +0300

    5.3.4. Задан связанный список. Определить цикличность.[#139059]

---
+++ chapter_004/src/main/java/ru/job4j/list2/NodeLoop.java
@@ -0,0 +1,55 @@
+package ru.job4j.list2;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.function.Consumer;
+
+public class NodeLoop<T> implements Iterable<T> {
+    private T value;
+    private Node<T> next;
+    private int modCount;
+
+    boolean hasCycle(Node first) {
+        Node fast = first;
+        Node slow = first;
+
+        while(fast.next != null && fast.next.next != null && slow.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow)
+                return true;
+        }
+        return false;
+    }
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+        Node(T data) {
+            this.data = data;
+        }
+    }
+    @Override
+    public Iterator <T> iterator() {
+        return new Iterator <T>() {
+            private Node<T> node = next;
+            private final int expectedModCount = modCount;
+            @Override
+            public boolean hasNext() {
+                if (modCount != expectedModCount)
+                    throw new ConcurrentModificationException();
+                return node != null;
+            }
+            @Override
+            public T next() {
+                if(!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                T result = node.data;
+                node = node.next;
+                return result;
+            }
+        };
+    }
+}
+
