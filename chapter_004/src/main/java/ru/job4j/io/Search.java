4f3211116c4959e658bd807c37d2eb8521fd57d5
imaginegeek57 <imaginegeek57@gmail.com>
2019-08-21 23:03:06 +0300

    3. Сканирование файловой системы.[#139093]

--- chapter_004/src/main/java/ru/job4j/io/Search.java
+++ chapter_004/src/main/java/ru/job4j/io/Search.java
@@ -2,27 +2,30 @@ package ru.job4j.io;
 
 import org.apache.commons.io.FilenameUtils;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Search {
 
-    public static List <File> searchFiles(String parent, List <String> exts) {
-
-        parent = "C/projects/somethingElse/cats.txt"; //
-        File[] files = new File(parent).listFiles(); //возвращает массив файлов и подкаталогов которы по пути parent
-        for (File f : files) {   // проверяем в цикле все файлы и папки
-            if (f.isDirectory()) { //если объект является каталогом то получаем его имя + обозначение
-                System.out.println(f.getName() + "folder");
-            } else {
-                System.out.println(f.getName() + "file");  //если файл является каталогом то получаем его имя + обозначение
+    public static List <File> searchFiles(String parent, List <String> exts) throws IOException {
+        List <String> fileList = new ArrayList <>();
+        List <String> dirList = new ArrayList <>();
+        parent = "C/projects/somethingElse/cats.txt";
+        File file = new File(parent);
+        if (file.isDirectory()) {
+            for (File item : file.listFiles()) {
+                if (item.isDirectory()) {
+                    dirList.add(item.getName());
+                } else {
+                    fileList.add(item.getName());
+                }
             }
         }
-        return null;   // хотел сделать метод void но idea ругается, возращать ноль как-то тупо но умнее ничего не придумал)
+        return null;
     }
 }
