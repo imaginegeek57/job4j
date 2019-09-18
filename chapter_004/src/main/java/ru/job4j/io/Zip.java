f96c2668520e0bb600270c5772b1cf11b2af3be8
imaginegeek57 <imaginegeek57@gmail.com>
2019-09-18 23:24:05 +0300

    4. Архивировать проект. [#139096]

---
+++ chapter_004/src/main/java/ru/job4j/io/Zip.java
@@ -0,0 +1,28 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Zip {
+
+    public void pack(File source, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            zip.putNextEntry(new ZipEntry(source.getPath()));
+            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+                zip.write(out.readAllBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+//    public List <File> seekBy(String root, String ext) {
+//        return ;
+//    }
+
+    public static void main(String[] args) {
+        new Zip().pack(new File("./chapter_004/pom.xml"), new File("./chapter_004/pom.zip"));
+    }
+}
