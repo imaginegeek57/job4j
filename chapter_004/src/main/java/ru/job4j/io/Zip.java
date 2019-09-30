ee888674e0457556aebad34c685f04a7208d34bd
imaginegeek57 <imaginegeek57@gmail.com>
2019-09-30 23:17:01 +0300

    4. Архивировать проект. [#139096]

--- chapter_004/src/main/java/ru/job4j/io/Zip.java
+++ chapter_004/src/main/java/ru/job4j/io/Zip.java
@@ -1,6 +1,5 @@
 package ru.job4j.io;
 
-import org.apache.commons.io.filefilter.WildcardFileFilter;
 import java.io.*;
 import java.util.*;
 import java.util.zip.ZipEntry;
@@ -8,10 +7,9 @@ import java.util.zip.ZipOutputStream;
 
 public class Zip {
 
-    private List <File> scan(String parent) {
+    private List <File> scan(File file) {
         List <File> fileList = new ArrayList <>();
         Queue <File> dirList = new LinkedList <>();
-        File file = new File(parent);
         if (file.isDirectory()) {
             dirList.add(file);
             while (!dirList.isEmpty()) {
@@ -26,20 +24,11 @@ public class Zip {
         return fileList;
     }
 
-    public List <File> seekBy(String root, String ext) {
-        List <File> search = scan(root);
-        FileFilter fileFilter = new WildcardFileFilter(ext);
-        while (search.isEmpty() == true) {
-            for (File f : search)
-                search.addAll(Arrays.asList(f.listFiles(fileFilter)));
-        }
-        return search;
-    }
-
     public void pack(File source, File target) {
+        List <File> fileList = new ArrayList <>(scan(source));
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            zip.putNextEntry(new ZipEntry(source.getPath()));
-            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+            zip.putNextEntry(new ZipEntry(fileList.get(0).getPath()));
+            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(fileList.get(0)))) {
                 zip.write(out.readAllBytes());
             }
         } catch (Exception e) {
