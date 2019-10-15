8cd5a8a0f36a7b7065b311e8ab603868e14a3676
imaginegeek57 <imaginegeek57@gmail.com>
2019-10-15 23:45:28 +0300

    5. Создать программу 'Консольный чат'. [#139097]

--- chapter_004/src/main/java/ru/job4j/IOChat/SimpleChat.java
+++ chapter_004/src/main/java/ru/job4j/IOChat/SimpleChat.java
@@ -1,5 +1,6 @@
 package ru.job4j.IOChat;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SimpleChat {
@@ -7,18 +8,16 @@ public class SimpleChat {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("input words: ");
-        String words = scanner.nextLine();
+        ArrayList<String> list = new ArrayList <>();
         String str = null;
         while ((str = scanner.nextLine()) != null) {
             if (str.equals("stop")) {
                 break;
-            } else {
-                continue;
             }
+            list.add(str);
         }
-        System.out.printf("words: %s \n", words);
+        System.out.printf("words: %s \n", list);
         scanner.close();
-
 
     }
 }
