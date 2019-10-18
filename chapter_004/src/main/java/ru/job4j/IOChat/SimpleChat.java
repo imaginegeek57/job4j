795cc5043e09219789366a416d4cfa2c7f96d527
imaginegeek57 <imaginegeek57@gmail.com>
2019-10-19 00:21:03 +0300

    5. Создать программу 'Консольный чат'. [#139097]

--- chapter_004/src/main/java/ru/job4j/IOChat/SimpleChat.java
+++ chapter_004/src/main/java/ru/job4j/IOChat/SimpleChat.java
@@ -4,39 +4,46 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class SimpleChat {
 
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("input words: ");
-//        ArrayList <String> list = new ArrayList <>();
-//        String str = null;
-//        while ((str = scanner.nextLine()) != null) {
-//            if (str.equals("stop")) {
-//                break;
-//            }
-//            list.add(str);
-//        }
-//        System.out.printf("words: %s \n", list);
-//        scanner.close();
-//    }
-
-
-    public void readingFile() {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("input words: ");
         ArrayList <String> list = new ArrayList <>();
-        try (BufferedReader br = new BufferedReader(new FileReader("answers"))) {
+        String str = null;
+        while ((str = scanner.nextLine()) != null) {
+            if (str.equals("stop")) {
+                break;
+            }
+            list.add(str);
+        }
+        readingAnswer();
+
+        System.out.printf("words: %s \n", list.get(random()));
+        scanner.close();
+    }
+
+    public static void readingAnswer() {
+        ArrayList <String> list = new ArrayList <>();
+        try (BufferedReader br = new BufferedReader(new FileReader("randomList.txt"))) {
             String str = null;
             while ((str = br.readLine()) != null) {
-                if (str.equals("stop")) {
-                    list.add(str);
-                }
+                list.add(str);
             }
+            System.out.println(list.get(random()));
         } catch (IOException e) {
             e.printStackTrace();
             System.err.println("Error: " + e.getMessage());
         }
+    }
+
+    public static int random() {
+        Random random = new Random();
+        int rnd = random.nextInt(6) + 1;
+        return rnd;
     }
 }
 
